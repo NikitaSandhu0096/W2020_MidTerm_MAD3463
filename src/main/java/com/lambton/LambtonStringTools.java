4f12964488aea7c0d4ee618754c0e3d0caa6a294
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-01-29 19:09:31 -0500

    [Nikita Sandhu] commit3

--- src/main/java/com/lambton/LambtonStringTools.java
+++ src/main/java/com/lambton/LambtonStringTools.java
@@ -34,13 +34,7 @@ public class LambtonStringTools
     //3 - FIND MOST FREQUENT CHARACTER FROM STRING
     public static String mostFrequent(String s) 
     {
-        char[] c1 = s.toCharArray();
-        int length = s.length();
-        for(int i=0; i<length; i++){
-            if(c1[i]==c1[i+1]){
-                System.out.println(c1);
-            }
-        }
+        
         return null;
     }
     
